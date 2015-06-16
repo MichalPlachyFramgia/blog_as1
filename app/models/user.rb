5a5db4391769ab1f57e346256f75c84c80993679
michalplachyFramgia <michal.plachy@framgia.com>
2015-06-16 13:27:57 +0700

    config

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,13 +79,9 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
-  end
-  
-  def feed
     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
+    Entry.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
   end
   # Follows a user.
